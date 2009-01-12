8fe178fad166a4a9aabde34ac4843ba2d4288b2f
mat <mat@c3c329ff-2060-0410-8d22-9a0ab058926f>
2009-01-12 00:47:35 +0000

    DM added
    git-svn-id: http://wakari.svnrepository.com/svn/islapeu/trunk@74 c3c329ff-2060-0410-8d22-9a0ab058926f

--- islapeu/slap/forms.py
+++ islapeu/slap/forms.py
@@ -44,6 +44,7 @@ class FullSlapForm(djangoforms.ModelForm):
         if verify: 
             slapee = self.cleaned_data.get('slapee')
             status = api.PostUpdate('TWITSLAPPED @%s - Find out why or slapback at http://www.islap.eu/%s/' % (slapee, slapee))
+            status = api.PostDirectMessage('TWITSLAPPED you - Find out why or slapback at http://www.islap.eu/%s/' % (slapee))
             self.cleaned_data['slaper_image_url'] = verify['profile_image_url']
         else:
             raise forms.ValidationError('Invalid Username and Password')
